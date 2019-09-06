a7571f64b9ddd4863f34610321c1cca74a4d679c
sherlock wisdom <wisdomnji@gmail.com>
2019-09-06 19:12:59 +0100

    added a socket manager called butler, helps maintain socket sanity

--- server/main.js
+++ server/main.js
@@ -3,8 +3,87 @@ const JsonSocket = require('json-socket');
 const Queue = require ('./../globals/queue.js');
 
 
+class SocketButler {
+//for you see, I'm simply one hell of a butler
+	constructor() {
+		this.socketContainer = [];
+		this.pendingClientConnect = {};
+	}
+
+	size() {
+		return this.socketContainer.length
+	}
+
+	addClientSocket(socket) {
+		this.socketContainer.push( socket );
+	}
+
+	removeClientSocket(socketUUID, clientToken) {
+		return new Promise( resolve => {
+			for(let i=0;i<this.socketContainer.length;++i) {
+				let socket = this.socketContainer[i];
+				console.log("remove::client::checking=>", socketUUID, " against ", socket.UUID, "at index: ", i);
+				if(socket.UUID == socketUUID && socket.clientToken == clientToken){
+					socket.end();
+					this.socketContainer.splice(i, 1);
+					--i;
+					//Rather unfor
+				}
+			}
+			resolve();
+		});
+	}
+
+	request(message, clientToken) {
+		return new Promise( async (resolve,reject) => {
+			try{
+				let clientSockets = await this.findClientSockets( clientToken );
+				if(clientSockets.length == 0) {
+					throw new Error("no client found");
+					reject();
+				}
+				if(clientSockets.length > 1) {
+					let requestType = message.type;
+					for(let i in clientSockets)
+						if(clientSockets[i].primaryType == requestType){ 
+							clientSockets[i].sendMessage(message);
+							resolve();
+						}
+				} 
+
+				else {
+					clientSockets[0].sendMessage(message);
+					resolve();
+				}
+			}
+			catch(error) {
+				console.log("SocketButler:request:error=> ", error.message);
+				reject();
+			}
+		});
+	}
+
+	storePendingClient( message, clientToken) {
+		//if(pendingMessageContainer
+		pendingMessageContainer[clientToken].insert( message );
+	}
+
+	async forward(message, clientToken) {
+		try {
+			let responds = this.request(message, clientToken);
+		}
+		catch(error) {
+			console.log("SocketButler:forward:error=> ", error.message);
+			//this.stateQueue.hardInsert( error );
+			this.storePendingClient( message, clientToken);
+		}
+	}
+}
+
+
 
 var socket = new Socket.Server();
+var socketButler = new SocketButler;
 var options = {
 	port : "8080",
 	host : "localhost"
@@ -27,7 +106,19 @@ socket.on('connection', ( socketClient )=>{
 				case "auth":
 				case "Auth":
 				case "AUTH":
-					console.log("socket:on:message=> authentication message");
+					if(!jsData.hasOwnProperty("UUID") || !jsData.hasOwnProperty("clientToken")) {
+						console.log("socket:on:message=> not a valid socket client");
+						//socketClient.sendEndMessage("who tf are you??"); got an endless loop, find another safe way to kill
+						throw new Error("invalid client");
+						return;
+					}
+					else {
+						console.log("socket:on:message=> socket authentication token:", jsData.clientToken);
+						socketClient.clientToken = jsData.clientToken;
+						socketClient.UUID = jsData.UUID;
+						socketButler.addClientSocket(socketClient);
+						console.log("socket:on:message=> number of client sockets:", socketButler.size());
+					}
 				break;
 
 				default:
@@ -40,7 +131,14 @@ socket.on('connection', ( socketClient )=>{
 		}
 	})
 
-	socketClient.on("close", ()=>{
+	socketClient.on("close",async ()=>{
 		console.log("socket:on:disconnect=> a socket just disconnected");
+		await socketButler.removeClientSocket(socketClient.UUID, socketClient.clientToken)
+		console.log("socket:on:close=> number of client sockets:", socketButler.size());
 	})
 })
+
+//TODO: check up who keeps the keep alive message, if not... there's not need for that
+//TODO: client socket.uuid(assigned after connection) is linked to socket.token (gotten after connection)
+//TODO: when request is made(iterating through uuid to get socket with requested token and request is forwarded)
+//TODO: @cool idea@ after doing the above, all sockets with token are gotten and each socket is checked for [request type] and message is forwarded if message and request type are same - default, if only 1 socket send message regardless, (if more than 1 socket - shit men, this is just cool)
