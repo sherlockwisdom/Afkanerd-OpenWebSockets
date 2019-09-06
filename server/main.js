afcde5b6863cdd2cdc5592e49280b28e9fb923aa
sherlock wisdom <wisdomnji@gmail.com>
2019-09-06 15:17:13 +0100

    added sebastian in case connections are lost, it brings them back... it makes the connection very persistentgit add .

--- server/main.js
+++ server/main.js
@@ -1,1 +1,46 @@
+const Socket = require ('net');
+const JsonSocket = require('json-socket');
+const Queue = require ('./../globals/queue.js');
 
+
+
+var socket = new Socket.Server();
+var options = {
+	port : "8080",
+	host : "localhost"
+}
+
+
+socket.listen(options, ()=>{
+	console.log("socket.listen=> listening on port ", options.port);
+})
+
+
+socket.on('connection', ( socketClient )=>{
+	console.log("socket.connection=> new connection made");
+	socketClient = new JsonSocket ( socketClient );
+
+	socketClient.on("message", ( jsData )=>{
+		try{
+			//Important note, messages coming in here a Json Object, do not need further parsing
+			switch( jsData.type ) {
+				case "auth":
+				case "Auth":
+				case "AUTH":
+					console.log("socket:on:message=> authentication message");
+				break;
+
+				default:
+					throw new Error("invalid message type");
+				break;
+			}
+		}
+		catch(error) {
+			console.log("socket:on:message:error=> ", error.message);
+		}
+	})
+
+	socketClient.on("close", ()=>{
+		console.log("socket:on:disconnect=> a socket just disconnected");
+	})
+})
