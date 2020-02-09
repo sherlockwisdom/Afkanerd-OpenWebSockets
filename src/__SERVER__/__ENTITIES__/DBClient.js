81786663b8519389828c3d8d42fafdcbeb9c08e6
Sherlock Wisdom <wisdomnji@gmail.com>
2020-02-09 17:11:56 +0100

    added lots of structures, this is gonna be hell to finish up

--- src/__SERVER__/__ENTITIES__/DBClient.js
+++ src/__SERVER__/__ENTITIES__/DBClient.js
@@ -6,7 +6,33 @@ class DBCLIENT {
 		this.__TOKEN__ = __TOKEN__;
 	}
 
-	validate( __ID__, __TOKEN__ ) {}
+	async validate( __ID__, __TOKEN__ ) {
+		return new Promise((resolve, reject)=>{
+			let query = "SELECT __ID__, __TOKEN__ FROM __DEKU_CLIENT__ WHERE __DEKU_CLIENT__.__ID__ = ? AND __DEKU_CLIENT__.__TOKEN__ = ?";
+			MySQLConnection.query( query, [ __ID__, __TOKEN__ ], (results, error)=>{ //TODO: Not sure about this
+				if( error ) {
+					//TODO: Only notice
+					reject();
+				}
 
-	validateTokenOnly( __TOKEN__ ) {}
+				if( result[0].__ID__ == __ID__ && result[0].__TOKEN__ == __TOKEN__ ) resolve( true );
+				resolve( false );
+			})	
+		});
+	}
+
+	async validateTokenOnly( __TOKEN__ ) {
+		return new Promise((resolve, reject)=>{
+			let query = "SELECT __ID__ FROM __DEKU_CLIENT__ WHERE __DEKU_CLIENT__.__TOKEN__ = ?";
+			MySQLConnection.query( query, [ __TOKEN__ ], (results, error)=>{ //TODO: Not sure about this
+				if( error ) {
+					//TODO: Only notice
+					reject();
+				}
+
+				if( result[0].__ID__ == __ID__ && result[0].__TOKEN__ == __TOKEN__ ) resolve( true );
+				resolve( false );
+			})	
+		});
+	}
 }
