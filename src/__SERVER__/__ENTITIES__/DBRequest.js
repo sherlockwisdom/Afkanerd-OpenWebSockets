e69a34a980a4343be0702f829a398d3f6dcf90f7
Sherlock Wisdom <wisdomnji@gmail.com>
2020-02-13 10:01:23 +0100

    can now bulk insert

--- src/__SERVER__/__ENTITIES__/DBRequest.js
+++ src/__SERVER__/__ENTITIES__/DBRequest.js
@@ -1,4 +1,3 @@
-
 module.exports = 
 class DBREQUEST {
 	constructor( __MYSQL_CONNECTION__, __ID__  ) {
@@ -13,11 +12,12 @@ class DBREQUEST {
 	get data() {}
 
 	insert(__USER_ID__, __SMS_COLLECTION__) {
-		for(let i=0;i<__SMS_COLLECTION__;++i) 
-			__SMS_COLLECTION__[i]["__USER_ID__"] = __USER_ID__;
+		let smsRequest = [];
+		for(let i=0;i<__SMS_COLLECTION__.length;++i)
+			smsRequest.push([__USER_ID__, __SMS_COLLECTION__[0].__MESSAGE__, __SMS_COLLECTION__[0].__PHONENUMBER__]);
 		return new Promise((resolve,reject)=>{
 			let query = "INSERT INTO __DEKU__.__REQUEST__ (__USER_ID__, __MESSAGE__, __PHONENUMBER__) VALUES ?";
-			this.__MYSQL_CONNECTION__.query(query, [__USER_ID__, [__SMS_COLLECTION__]], ( error, result )=>{
+			this.__MYSQL_CONNECTION__.query(query, [smsRequest], ( error, result )=>{
 				if( error ) {
 					console.log( error );
 					reject( error );
