5174b815736d52c99130e7c71a06591acd60ee3c
Sherlock Wisdom <wisdomnji@gmail.com>
2020-02-16 17:53:39 +0100

    file structures added

---
+++ src/__CLIENT__/start.js
@@ -0,0 +1,47 @@
+const bodyParser = require('body-parser')
+
+const START_ROUTINES = require('./start_routines.js');
+
+var __DBCLIENT__ = require('./__ENTITIES__/DBClient.js');
+var __DBREQUEST__ = require('./__ENTITIES__/DBRequest.js');
+var SOCKETS = require('./__ENTITIES__/Socket.js');
+
+//===============
+'use strict';
+//===============
+
+//=======================================================
+let CONFIGS = START_ROUTINES.READCONFIGS('system_configs');
+let RETURN_VALUES = START_ROUTINES.READCONFIGS('return_values');
+if(typeof CONFIGS["__DEFAULT__"] == "undefined") {
+	console.error("=> CONFIGS NOT PROPERLY LOADED");
+	return;
+}
+//=======================================================
+
+//================================================
+var __MYSQL_CONNECTION__;
+var __SOCKET_COLLECTION__;
+SOCKETS = new SOCKETS;
+
+(async ()=>{
+	try{
+		__MYSQL_CONNECTION__ = await START_ROUTINES.GET_MYSQL_CONNECTION();
+		console.log("=> MYSQL CONNECTION ESTABLISHED");
+	}
+	catch(error) {
+		console.log(error);
+		return;
+	}
+})();
+
+(async ()=>{
+	try {
+		__SOCKET_COLLECTION__ = await SOCKETS.startSockets();
+		console.log("=> SOCKETS ESTABLISHED");
+	}
+	catch( error ) {
+		console.log(error);
+		return;
+	}
+})();
---
+++ src/__CLIENT__/start_routines.js
@@ -0,0 +1,43 @@
+const __MYSQL_CONNECTION__ = require ( 'mysql' );
+
+'use strict'
+
+module.exports = {
+	READCONFIGS : ( config )=> {
+		let path = "";
+		switch( config ) {
+			case 'system_configs':
+				path = "__COMMON_FILES__/system_configs.env";
+				require('dotenv').config({path: path.toString()})
+				
+				return process.env;
+			break;
+
+			case 'return_values':
+				path = "__COMMON_FILES__/return_values.env";
+				require('dotenv').config({path: path.toString()})
+
+				return process.env;
+			break;
+		}
+	},
+
+	GET_MYSQL_CONNECTION : ()=> {
+		return new Promise ( (resolve, reject) => {
+			let path = "__COMMON_FILES__/mysql.env";
+			require('dotenv').config({path: path.toString()})
+			try{
+				let mysql_connection = __MYSQL_CONNECTION__.createConnection({
+					host : process.env.MYSQL_HOST,
+					user : process.env.MYSQL_USER,
+					password : process.env.MYSQL_PASSWORD,
+					database : process.env.MYSQL_DATABASE
+				});
+				resolve(mysql_connection);
+			}
+			catch(error) {
+				reject(error)
+			}
+		});
+	}
+}
