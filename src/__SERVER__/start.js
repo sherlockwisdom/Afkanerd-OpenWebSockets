85525966f017a5f5c8f237871e1c9520e87e704e
Wisdom Nji <wisdomnji@gmail.com>
2020-03-21 22:39:01 +0100

    incoming API request being processed good!

--- src/__SERVER__/start.js
+++ src/__SERVER__/start.js
@@ -8,7 +8,7 @@ var MySQLConnector = require('./../MYSQL_CONNECTION.js');
 
 'use strict';
 let configs = {
-	COMPONENT : 'SMS',
+	COMPONENT : '/sms',
 	SOCKET_PORT : '3000',
 	API_PORT : '8000'
 }
@@ -21,10 +21,13 @@ let return_values = {
 }
 
 const mysql_env_path = "__COMMON_FILES__/mysql.env";
+const APIOptions = {
+	port : '8000'
+}
 var mysqlConnection;
 var sockets;
-
 var app = express();
+
 app.use(bodyParser.json({limit: '50mb'}));
 app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 
@@ -52,75 +55,22 @@ app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 })();
 
 //=================================
-let APIOptions = {
-	port : '8000'
-}
 
 app.listen(APIOptions, ()=>{
 	console.log("=> RECEIVING API BEGAN, RUNNING ON PORT [%d]", APIOptions.port);
 });
 
 app.post(configs.COMPONENT, async (req, res)=>{
-	let __BODY__ = req.body;
+	let request_body = req.body;
+	console.log(request_body);
 
-	console.log(__BODY__);
-
-	let __CLIENT__ = __BODY__.__CLIENT__;
-
-	if(
-		typeof __CLIENT__["ID"] == "undefined" ||
-		typeof __CLIENT__["TOKEN"] == "undefined"
-	) {
-		console.log("-- Invalid request made --");
-		res.status( RETURN_VALUES['INVALID_REQUEST'] );
-		res.end();
+	if( !Array.isArray( request_body ) ) {
+		console.error("=> NOT VALID REQUEST!");
+		res.status(400).end();
+		return;
 	}
 
-	let __USER_ID__ = __CLIENT__.ID;
-	let __TOKEN__ = __CLIENT__.TOKEN;
-
-	//Let's validate this client
-	let DBDeku = new __DBCLIENT__( __MYSQL_CONNECTION__, __USER_ID__, __TOKEN__);
-
-	try{
-		let validated_client = await DBDeku.validate(__USER_ID__, __TOKEN__);
-		if( !validated_client) {
-			res.status( RETURN_VALUES['NOT_AUTHORIZED'] );
-			res.end();
-		}
-	}
-	catch( error ) {
-		console.log(error);
-		res.end();
-	}
-	
-	//Now let's validate the request
-	let __REQUEST__ = __BODY__.__REQUEST__;
-	if(
-		typeof __REQUEST__["__REQUEST__"] == "undefined"
-	) {
-		res.status( RETURN_VALUES['INVALID_REQUEST'] ).end();
-	}
-
-	let __SMS_COLLECTION__ = __REQUEST__.__SMS__;
-
-	let DBRequest = new __DBREQUEST__(__MYSQL_CONNECTION__);
-
-	if( !DBRequest.valid( __SMS_COLLECTION__ ) ) {
-		console.log("=> REQUEST NOT WELL FORMED");
-		res.status(RETURN_VALUES['INVALID_REQUEST']).end();
-	}
-	else {
-		let DBRequestID = await DBRequest.insert(__USER_ID__, __SMS_COLLECTION__);
-		console.log("=> %d REQUEST STORED IN DATABASE", DBRequestID.affectedRows);
-		let __SOCKET__ = await __SOCKET_COLLECTION__.find(__USER_ID__, __TOKEN__, __MSG_ID__);
-		if( !__SOCKET__.transmit( __SMS_COLLECTION__ ) ){
-			res.status(__SOCKET__.getErrorCode() );
-			res.end();
-		}
-		
-		res.status( RETURN_VALUES['FAILED'] );
-		res.end();
-	}
+	console.log("=> PROCESSING NEW REQUEST");
+	res.status(200).end();
 });
 
