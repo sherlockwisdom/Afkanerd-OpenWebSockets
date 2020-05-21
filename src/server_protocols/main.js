abcb627ebbb3fb9187635fe289414615fdbd829d
Sherlock Wisdom <wisdomnji@gmail.com>
2020-05-21 09:17:24 +0100

    template done

--- src/server_protocols/main.js
+++ src/server_protocols/main.js
@@ -4,20 +4,17 @@
 var CONFIG_PATH
 
 if( process.argv.length < 3 ) {
-	// something important goes here
-	
+	// TODO, put some important message here to make the user aware of what's happening
 	return;
 }
 
 for( let i in process.argv ) {
-	// console.log( process.argv[i] );
 	if( process.argv[i] == "--c" ) {
 		// TODO, check in here to make sure the next things are the required arguments
 		CONFIG_PATH = process.argv[Number(i)+1];
 	}
 }
 
-console.log( CONFIG_PATH )
 require('dotenv').config( { path: CONFIG_PATH } )
 const boot = require('./boot.js')
 
